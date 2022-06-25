38f9f984884171e2c6ddb47718869f6ae72e13b5
Santiago Efrain Martinez Leon <60367880+Cholado@users.noreply.github.com>
2022-06-25 09:20:52 -0500

    set up handlebars engine

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@
 const express = require('express');
 // set up request logger middleware
 const morgan = require('morgan');
+// set up logicless templating language from express
+const exphbs = require('express-handlebars');
+// set up path
+const path = require('path');
+const { extname } = require('path');
 
 // initializations
 /*
@@ -11,7 +16,24 @@ The code below will execute express
 const app = express();
 
 // settings (express server config)
+/*
+The code below set up
+localhost port:
+*/
 app.set('port', process.env.PORT || 4000);
+/*
+The code below set up
+handlebars engine path for views:
+*/
+app.set('views', path.join(__dirname, 'views'))
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    helpers: require('./lib/handlebars')
+}));
+app.set('view engine', '.hbs');
 
 // middlewares
 /*
@@ -23,7 +45,7 @@ app.use(morgan('dev'));
 // global variables
 
 // routes
-app.use(require('./routes/index.js'))
+app.use(require('./routes/index.js'));
 // public
 
 // server trigger
